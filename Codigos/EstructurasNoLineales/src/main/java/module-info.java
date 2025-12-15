d6b1389e200498d1c8b27af38f0b316bc144fa1e
jhonyzars-hash <jhonyzars@gmail.com>
2025-12-15 01:28:37 -0600

    Eliminar actividades de Unidad 4 para restaurar Unidad 3

--- Codigos/EstructurasNoLineales/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.jhonysiystem.estructurasnolineales {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.jhonysiystem.estructurasnolineales.controller to javafx.fxml;
-    exports com.jhonysiystem.estructurasnolineales;
-}
